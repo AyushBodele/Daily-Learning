aa8acb61719de172628a0943ed0a5a450ebcabe0
AyushBodele <ayushbodele83@gmail.com>
2025-06-09 13:02:58 +0530

    promise.reolve add

--- async.js
+++ async.js
@@ -2,9 +2,14 @@ const fs = require("fs");
 const a = 100;
 setImmediate(() => console.log("setImmediate"));
 fs.readFile("file.txt", () => console.log("File Reading CB"));
+
+Promise.resolve().then(() => console.log("Promise resolved"));
+process.nextTick(() => console.log("Next Tick Callback"));
+
 setTimeout(() => console.log("Timer expired"), 0);
 function printA() {
   console.log("a=", a);
 }
 printA();
+process.nextTick(() => console.log("Next Tick Callback"));
 console.log("Last line of the file.");
