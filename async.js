b8d9ee964ef9e3651262e63ae3c22867268585f9
AyushBodele <ayushbodele83@gmail.com>
2025-06-10 23:33:42 +0530

    more nested

--- async.js
+++ async.js
@@ -6,7 +6,17 @@ setImmediate(() => console.log("setImmediate"));
 
 fs.readFile("file.txt", () =>{ 
     console.log("File read completed");
-    process.nextTick(() => console.log("Next Tick 2"));
+    process.nextTick(() => {
+        process.nextTick(() => {
+        process.nextTick(() => console.log("Inner Next Tick"));
+        console.log("Next Tick 1");
+        Promise.resolve().then(() => {
+        process.nextTick(() => console.log("Nested Inner Next Tick"));
+        Promise.resolve().then(() => console.log(" Inner Promise resolved"));
+    });
+});
+        console.log("Next Tick 2")}
+    );
     setImmediate(() => console.log("setImmediate 2"));
 });
 
